1f61b6901f408ea041ae94f362687b9f0e33371f
Somesh Kar <somesh.kar@gmail.com>
2020-10-04 20:31:07 +0530

    Clean up

---
+++ crypto/rsa_madness-SOLVED/solve.py
@@ -0,0 +1,28 @@
+from pwn import *
+from Crypto.Util.number import *
+
+context.log_level = "error"
+
+e = 65537
+r = process("rsa.py")
+r.recvuntil("\n")
+c = int(r.recvuntil("\n").strip().split("- ")[1])
+r.recvuntil('Exit\n')
+r.sendline("2")
+r.recvuntil('\n')
+
+# retrieve N with input: -1
+r.sendline("-1")
+n = int(r.recvuntil('\n').strip()) + 1
+
+
+# start retrieving flag
+r.recvuntil("Exit\n")
+r.sendline("2")
+
+
+mul = pow(2, e, n)
+payload = c*mul % n
+r.sendline(str(payload))
+flag = int(r.recvuntil('\n').strip())
+print "Flag:", long_to_bytes(flag/2)
--- crypto/rsa_madness/mod.py
+++
@@ -1,5 +0,0 @@
-import gmpy2
-
-n = int(input())
-
-print(gmpy2.powmod(2, 65537, n))
